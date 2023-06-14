8ec09d393d218b9a199adb5ecbb401edaaf47d10
C Triis <vothanhtri.work@gmail.com>
2023-06-14 11:53:31 +0700

    promotion models

---
+++ controllers/promotionController.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const Promotion = require('../models/promotion').Promotion
+
+function PromotionController() {
+    const SELF = {};
+    return {
+        getList: (req, res) => {
+            
+        }
+    }
+}
+
+
+module.exports = new PromotionController();
---
+++ models/promotion.js
@@ -0,0 +1,22 @@
+"use strict"
+const mongoose =require("mongoose");
+
+const Promotion = mongoose.Schema(
+    {
+        promo_name: { type: String},
+        promo_percent: {type: String},
+        start_date: {type: String},
+        end_date: {type: String},
+        productIds: {type: String},
+        promo_des: {type: String}
+    },
+    { versionKey: false, timestamps: true }
+);
+
+Promotion.stactics.objectId = function (id) {
+    return mongoose.Types.ObjectId(id);
+};
+
+module.exports = {
+    Promotion: mongoose.model("promotion", Promotion),
+};
