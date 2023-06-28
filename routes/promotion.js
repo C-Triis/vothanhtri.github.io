508ddd5435795128247a254ef71f6616d7cfcbf1
C Triis <vothanhtri.work@gmail.com>
2023-06-28 21:10:59 +0700

    update date

--- routes/promotion.js
+++ routes/promotion.js
@@ -4,7 +4,9 @@ const express = require('express');
 const router = express.Router({});
 const PromotionController = require('../controllers/promotionController');
 const ProductController = require('../controllers/producController')
-const Product =require('../models/product').Product
+const Product = require('../models/product').Product
+const DateServices = require('../services/date')
+
 router.get("/list", (req, res) => {
     let page = req.query.page;
     let keySearch = req.query.keySearch || "";
@@ -19,7 +21,21 @@ router.get("/list", (req, res) => {
             let productIds=rs[0][i].productIds
            let products =await Product.find({_id: {$in: productIds}}).lean()
            let productList = products.map(product => product.name_prod)
-           rs[0][i]['productList'] =productList.join(', ')
+           rs[0][i]['productList'] = productList.join(', ')
+           rs[0][i]['start'] =`${DateServices.getStrDate(
+            "DD/MM/YYYY",
+            new Date(rs[0][i].start_date)
+          )} ${DateServices.getStrTime(
+            "SS:MM:HH",
+            new Date(rs[0][i].start_date)
+          )}`
+          rs[0][i]['end'] =`${DateServices.getStrDate(
+            "DD/MM/YYYY",
+            new Date(rs[0][i].end_date)
+          )} ${DateServices.getStrTime(
+            "SS:MM:HH",
+            new Date(rs[0][i].end_date)
+          )}`
         }
         res.render("pages/admin/promotion", {
             promotion: rs[0],
